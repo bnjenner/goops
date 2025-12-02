2c3e7b3677f8552940d6e50aa2d23e292638bfdb
bnjenner <bnjenner@bu.edu>
2025-12-02 14:28:26 -0500

    Install fix

---
+++ build/lib/goops/goops.py
@@ -0,0 +1,19 @@
+import sys
+import argparse
+import logging
+import numpy as np
+from goops import utils
+from goops import motif
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Partitions sequences through  motifs discovery using the goops algorithm.")
+    parser.add_argument("-o", "--output-prefix", type=str, default='goops_output', help='Prefix for file outputs. (Default: goops_output)')
+    parser.add_argument("gtf", help="Input Fasta file.")
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
