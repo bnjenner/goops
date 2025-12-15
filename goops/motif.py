c241b5dc46915a4c3157b5ce241f52fb7b96f73e
Elm-trees <emarkert@smith.edu>
2025-12-15 09:53:00 -0500

    Adding learning for background model, weighting by KL divergence

--- goops/motif.py
+++ goops/motif.py
@@ -3,6 +3,7 @@ import math
 import logging
 import numpy as np
 from goops import utils
+from scipy.special import kl_div
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
@@ -31,7 +32,7 @@ class Goops:
         self.min_itererations = 5
         self.max_itererations = None
         self.pseudocount = 0.001
-        self.epsilon = 0.01
+        self.epsilon = 0.0001
         self.bases = {"A": 0, "C": 1, "G": 2, "T": 3}
 
         # Results
@@ -51,7 +52,7 @@ class Goops:
 
     ######################################################################
     # Evaluate Motif Likelihood
-    def motif_ll(self, seq: str, _motif: np.ndarray, start_pos: int = None):
+    def motif_ll(self, seq: str, _motif: np.ndarray, _background: np.ndarray,start_pos: int = None):
         
         ll = 0
         n = len(seq)
@@ -66,7 +67,7 @@ class Goops:
                 if pos >= z and pos <= end_pos:
                     ll += np.log(_motif[b][pos - z])
                 else:
-                    ll += math.log(0.25)
+                    ll += np.log(_background[b])
 
         else:
             for z in range(num_pos):
@@ -76,7 +77,7 @@ class Goops:
                     if pos >= z and pos <= end_pos:
                         ll += np.log(_motif[b][pos - z])
                     else:
-                        ll += math.log(0.25)
+                        ll += np.log(_background[b])
 
         return ll
 
@@ -98,7 +99,7 @@ class Goops:
             
             i = 0
             for g, m in self.results["Motifs"].items():
-                ll[i] = self.motif_ll(seq, m["Motif"])
+                ll[i] = self.motif_ll(seq, m["Motif"], self.results["Background"])
                 classifications[header]["LL_Group_" + str(i)] = str(round(ll[i], ndigits = 2))
                 i += 1
             
@@ -109,30 +110,39 @@ class Goops:
 
     ######################################################################
     # Initialize Motif Models
-    def __initialize_models(self, uniform: bool = False):
+    def __initialize_models(self, uniform: bool = False, zeroes: bool = False):
         
-        g_prob = 1 / self.num_groups
-        l_prob = 1 / self.num_lengths
-        n_prob = 0.25
+        if zeroes == True:
+            g_prob = 0
+            l_prob = 0
+            n_prob = 0
+        else:
+            g_prob = 1 / self.num_groups
+            l_prob = 1 / self.num_lengths
+            n_prob = 0.25
 
         # Uniform Priors
         _gamma  = np.full((self.num_groups, self.num_seqs), g_prob, dtype=np.float64)
         _lambda = np.full((self.num_groups, self.num_lengths), l_prob, dtype=np.float64)
         _motifs = [[] for g in range(self.num_groups)]
+        _background = [0.25, 0.25, 0.25, 0.25]
 
         # Initlize Motif Model for all Groups and Lengths
         for g in range(self.num_groups):
             if not uniform:
-                _motifs[g] = [utils.random_mat(4, l) for l in range(self.min_length, self.max_length+1)]
+                if not zeroes:
+                    _motifs[g] = [utils.random_mat(4, l) for l in range(self.min_length, self.max_length+1)]
+                else:
+                    _motifs[g] = [np.full((4, l), 0, dtype = np.float64) for l in range(self.min_length, self.max_length+1)]
             else:
                 _motifs[g] = [np.full((4, l), n_prob, dtype=np.float64) for l in range(self.min_length, self.max_length+1)]
 
-        return _motifs, _lambda, _gamma
+        return _motifs, _lambda, _gamma, _background
 
 
     ######################################################################
     # Expectation Maximization Goops Implementation
-    def __discover_EM(self, _gamma: np.ndarray, _lambda: np.ndarray, _motifs: list):
+    def __discover_EM(self, _gamma: np.ndarray, _lambda: np.ndarray, _motifs: list, _background: list):
 
         G = self.num_groups
         L = self.num_lengths
@@ -149,8 +159,8 @@ class Goops:
             # cont = input("Continue...")
 
             x = 0
-            _motifs_tp1, _lambda_tp1, _gamma_tp1 = self.__initialize_models(uniform = True)            
-            
+            _motifs_tp1, _lambda_tp1, _gamma_tp1, _background_tp1 = self.__initialize_models(zeroes = True)
+
             for header, seq in self.sequeces.items():
 
                 # print(header, _gamma[:,x])
@@ -162,8 +172,8 @@ class Goops:
 
                     # E Step 
                     for z in range(num_pos):         
-                        for g in groups:                  
-                            Q[g][z] += self.motif_ll(seq, _motifs[g][l], start_pos = z)
+                        for g in groups:     
+                            Q[g][z] += self.motif_ll(seq, _motifs[g][l], _background, start_pos = z)
                             Q[g][z] += np.log(_gamma[g][x]) + np.log(_lambda[g][l]) + np.log(1 / num_pos)
                         Q[:,z] =  utils.logsafe_normalize(Q[:,z])
                         # Q[g,:] = utils.logsafe_normalize(Q[g,:])
@@ -180,26 +190,38 @@ class Goops:
                             for m in range(lengths[l]):
                                 for n, b in self.bases.items():
                                     if seq[z+m] == n:
-                                        _motifs_tp1[g][l][b][m] += np.exp(Q[g][z])
+                                        _motifs_tp1[g][l][b][m] += (np.exp(Q[g][z]))
+                                        _background_tp1[b] += (1 - np.exp(Q[g][z]))
 
                 x += 1 # Incr seq count
-
+            
             # Noramlize models to sum to 1
             _gamma_tp1 /= np.sum(_gamma_tp1, axis = 0)
+            _background_tp1 /= np.sum(_background_tp1)
             for g in groups:
                 _lambda_tp1[g] /= np.sum(_lambda_tp1[g])
                 for m in range(len(_motifs_tp1[g])):
                     for c in range(_motifs_tp1[g][m].shape[1]):
-                         _motifs_tp1[g][m][:,c] /= np.sum(_motifs_tp1[g][m][:,c])
+                        _motifs_tp1[g][m][:,c] /= np.sum(_motifs_tp1[g][m][:,c])
+                        _IC = kl_div(_motifs_tp1[g][m][:,c], _background)
+                        _motifs_tp1[g][m][:,c] *= _IC
+                        _motifs_tp1[g][m][:,c] /= np.sum(_motifs_tp1[g][m][:,c])
 
+
+            """
+            12/15/25 EXM: 
+                Changed convergence to be based on motif model because of KL divergence side effects, but haven't experimented
+                with changing it back to be based on group parameters.
+            """
             # Check Convergence
-            diff = np.abs(np.array((_gamma_tp1 - _gamma)))
+            diff = np.abs(np.subtract(np.array(_motifs_tp1), np.array(_motifs)))
             if iterations > self.min_itererations and bool(np.all(diff < self.epsilon)):
                 converged = True
 
             _gamma = _gamma_tp1
             _lambda = _lambda_tp1
             _motifs = _motifs_tp1
+            print(_motifs)
             iterations += 1
 
 
@@ -213,7 +235,7 @@ class Goops:
         seq_ids = list(self.sequeces.keys())
         classifications = np.argmax(_gamma, axis=0)
         results = {"Groups": {seq: int(group) for seq, group in zip(seq_ids, classifications)},
-                   "Motifs": {}}
+                   "Motifs": {}, "Background": _background}
 
         for g in groups:
             results["Motifs"]["Group_" + str(g)] = {"Motif": _motifs[g][0], "LogLikelihood": ";)"}
@@ -254,11 +276,11 @@ class Goops:
                 I Imagine this is were we will implement better landscape exploration
             """
         
-            _motifs, _lambda, _gamma = self.__initialize_models()
+            _motifs, _lambda, _gamma, _background= self.__initialize_models()
 
             # Run Aglorithm
             if algo == "EM":
-                results = self.__discover_EM(_gamma, _lambda, _motifs)
+                results = self.__discover_EM(_gamma, _lambda, _motifs, _background)
             else:
                 print("ERROR: Algorithm " + algo + " is not implemented yet.")
                 sys.exit(1)
